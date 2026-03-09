99f6f1166ff66cf44d3316002e2383643b59a06e
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-03-09 10:44:29 +0530

    first commit

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class AppTest {
+    @Test
+    public void testAdd() {
+        App app = new App();
+        assertEquals(5, app.add(2, 3));
+    }
+}
